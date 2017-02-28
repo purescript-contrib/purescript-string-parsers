d75af2dd41bcea8cc6f1658985ee27440d8467d3
John Watson <john.watson@gmx.co.uk>
2017-02-28 18:31:27 +0000

    Revert to a single function named regex

--- src/Text/Parsing/StringParser/String.purs
+++ src/Text/Parsing/StringParser/String.purs
@@ -15,7 +15,6 @@ module Text.Parsing.StringParser.String
   , upperCaseChar
   , anyLetter
   , alphaNum
-  , regex'
   , regex
   ) where
 
@@ -116,38 +115,30 @@ anyLetter = lowerCaseChar <|> upperCaseChar <?> "Expected a letter"
 alphaNum :: Parser Char
 alphaNum = anyLetter <|> anyDigit <?> "Expected a letter or a number"
 
--- | Build the regular expression from the pattern and match it, ensuring
--- | that the pattern only attempts to match from the start of the target.
-regex' :: String -> Parser String
-regex' pat =
-    case Regex.regex pattern noFlags of
-      Left _ ->
-        fail $ "Text.Parsing.StringParser.String.regex': illegal regex " <> pat
-      Right r ->
-        regex r
-    where
-      pattern =
-        case stripPrefix (Pattern "^") pat of
-          Nothing ->
-            "^" <> pat
-          _ ->
-            pat
-
--- | Match the regular expression.
-regex :: Regex.Regex -> Parser String
-regex r =
-  Parser \{ str, pos } ->
-    let
-      remainder = drop pos str
-    in
-      -- reduce the possible array of matches to 0 or 1 elements to aid Array pattern matching
-      case uncons $ fromMaybe [] $ Regex.match r remainder of
-        Just { head: Just matched, tail: _ }  ->
-          -- only accept matches at position 0
-          case stripPrefix (Pattern matched) remainder of
-            Nothing ->
-              Left { pos, error: ParseError $ "Text.Parsing.StringParser.String.regex: no match - consider prefacing the pattern with '^'" }
-            _ ->
-              Right { result: matched, suffix: { str, pos: pos + length matched } }
+-- | match the regular expression
+regex :: String -> Parser String
+regex pat =
+  case Regex.regex pattern noFlags of
+    Left _ ->
+      fail $ "Text.Parsing.StringParser.String.regex': illegal regex " <> pat
+    Right r ->
+      matchRegex r
+  where
+    -- ensure the pattern only matches the current position in the parse
+    pattern =
+      case stripPrefix (Pattern "^") pat of
+        Nothing ->
+          "^" <> pat
         _ ->
-          Left { pos, error: ParseError $ "no match" }
+          pat
+    matchRegex :: Regex.Regex -> Parser String
+    matchRegex r =
+      Parser \{ str, pos } ->
+        let
+          remainder = drop pos str
+        in
+          case uncons $ fromMaybe [] $ Regex.match r remainder of
+            Just { head: Just matched, tail: _ }  ->
+              Right { result: matched, suffix: { str, pos: pos + length matched } }
+            _ ->
+              Left { pos, error: ParseError $ "no match" }
