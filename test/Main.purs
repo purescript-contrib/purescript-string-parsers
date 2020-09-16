3c11820f301e6130b131eb46f2961761db0f4cb9
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-09-15 18:23:00 -0700

    Remove CodePoints parser

--- test/Main.purs
+++ test/Main.purs
@@ -4,13 +4,9 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
-import Test.CodePoints (testCodePoints)
 import Test.CodeUnits (testCodeUnits)
 
 main :: Effect Unit
 main = do
-  log "Testing CodePoint parsing\n"
-  testCodePoints
-
   log "\n\nTesting CodeUnit parsing\n"
   testCodeUnits
