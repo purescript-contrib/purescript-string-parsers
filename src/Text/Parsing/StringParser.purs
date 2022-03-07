636d2f1518486a9c6d8c7be93cab68c10eaa932f
chtenb <ctenbrinke@gmail.com>
2022-03-07 21:00:09 +0100

    re-export other modules in main API

---
+++ src/Text/Parsing/StringParser.purs
@@ -0,0 +1,9 @@
+module Text.Parsing.StringParser.StringParser
+  ( module Text.Parsing.StringParser.Common
+  , module Text.Parsing.StringParser.Combinators
+  , module Text.Parsing.StringParser.CodePoints
+  ) where
+
+import Text.Parsing.StringParser.Common
+import Text.Parsing.StringParser.Combinators
+import Text.Parsing.StringParser.CodePoints
